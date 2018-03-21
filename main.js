aed991c96bd8b889e0e48462b6ee39174f89d9c4
brzk <barakraz6@gmail.com>
2018-03-21 23:29:04 +0200

    create server file
    Signed-off-by: brzk <barakraz6@gmail.com>

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('static_data'));
+app.set('port', (process.env.PORT || defaultPort));
+
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
